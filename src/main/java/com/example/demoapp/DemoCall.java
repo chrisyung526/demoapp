7e65735660549e7540f36d81e5624ea180dadb67
Chris Yung <chris.vtz250@gmail.com>
2023-03-15 13:55:56 +0800

    fix code smell

--- src/main/java/com/example/demoapp/DemoCall.java
+++ src/main/java/com/example/demoapp/DemoCall.java
@@ -5,8 +5,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
