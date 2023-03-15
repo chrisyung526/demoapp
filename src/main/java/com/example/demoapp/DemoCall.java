39552193066f96ff929f4e4a84e7adf27b819a77
Chris Yung <chris.vtz250@gmail.com>
2023-03-15 14:02:27 +0800

    @RefreshScope to the DemoCall

--- src/main/java/com/example/demoapp/DemoCall.java
+++ src/main/java/com/example/demoapp/DemoCall.java
@@ -4,10 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class DemoCall {
     @Value("${foo.bar}")
     private String foobar;
