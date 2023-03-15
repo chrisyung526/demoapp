faff455612cac2942d7fd291128f41736e4f9d97
Chris Yung <chris.vtz250@gmail.com>
2023-03-15 12:31:28 +0800

    add demo function to get properties value

--- src/main/java/com/example/demoapp/DemoappApplication.java
+++ src/main/java/com/example/demoapp/DemoappApplication.java
@@ -1,6 +1,7 @@
 package com.example.demoapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
@@ -10,14 +11,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class DemoappApplication {
 
+	@Value("${foo.bar}")
+	private String foobar;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoappApplication.class, args);
+	}
+
+	@RequestMapping("/demo")
+	public Map<String, String> demo() {
+
+		HashMap<String, String> map = new HashMap<>();
+		map.put("foo.bar",foobar);
+		return map;
 	}
 
 }
